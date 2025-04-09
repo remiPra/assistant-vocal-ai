78ad47184a6ad66451859778f6610c09d8fe21b6
remiPra <remipradere@gmail.com>
2025-04-09 22:22:14 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,15 @@ module.exports = {
   entry: './js/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),  // Changez 'dist' en 'public'
+    path: path.resolve(__dirname, 'public'),
   },
   mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true
+  }
 };
